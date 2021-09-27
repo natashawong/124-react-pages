7869d219f57e59546328c482912f37f118bea2ff
natashawong <natasha.wong10@gmail.com>
2021-09-27 14:08:29 -0700

    finish

--- react-gh-pages/src/App.js
+++ react-gh-pages/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
@@ -13,22 +14,48 @@ function App() {
       phone: "(909) 675 6412",
     },
   ]
+  const [clicked, setClick] = useState(-1)
+
+  const [opacity, setOpacity] = useState("50%");
+
+  useEffect(() => {
+    setInterval(() => {
+      setOpacity((v) => (v === "50%" ? "100%" : "50%"));
+    }, 1000);
+  }, []);
+
+  function ContactList(props) {
+
+    return props.data.map((obj, i) => 
+      <Contact 
+        name={obj.name}
+        phone={obj.phone} 
+        email={obj.email}
+        index={i}
+        style={{backgroundColor: clicked === i && 'yellow', opacity:  clicked === i && opacity}}
+        onClick={() => {clicked === i ? setClick(-1) : setClick(i)}}
+      />
+    )
+  }
+
+  function Contact(props) {
+    return (
+      <div className="container" style={props.style} onClick={props.onClick}>
+        <p><b>{props.name}</b></p>
+        <p className="email">{props.email}</p>
+        <p>{props.phone}</p>
+      </div>
+    )
+  }
+
   return (
     <>
       <div>
-        <h1>People</h1>
+        <h1>People ({clicked === -1 ? 0 : 1}/{data.length} selected)</h1>
       </div>
-
-      {data.map((obj) =>
-        <div className="container">
-          <p><b>{obj.name}</b></p>
-          <p className="email">{obj.email}</p>
-          <p>{obj.phone}</p>
-        </div>
-      )}
-
+      <ContactList data={data} />
     </>
-  );
+  )
 }
 
 export default App;
